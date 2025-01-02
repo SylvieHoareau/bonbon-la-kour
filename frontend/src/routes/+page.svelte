e460b35430de98b3204d4ab32777bbe73b6de7f9
hoareausylvie <hoareauylvie@outlook.fr>
2025-01-02 10:50:56 +0400

    build: configuration de Tailwind CSS

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    export let title = "Bonbon La Kour";
+</script>
+
+<section class="text-center py-16 bg-yellow-100">
+    <h1>Bienvenue chez {title}</h1>
+    <p>Découvrez les saveurs uniques des gâteaux créoles de La Réunion</p>
+    <a href="/gateaux" class="button">Voir nos gâteaux</a>
+</section>
+
+<style>
+
+    .button {
+        padding: 0.5rem 1rem;
+        background: #f4a261;
+        color: white;
+        text-decoration: none;
+        border-radius: 5px;
+    }
+
+    .button:hover {
+        background: #e76f51;
+    }
+</style>
+
---
+++ frontend/src/routes/gateaux/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let gateaux = [
+        { id: 1, name: "Gâteau Tisson", price: "20€"},
+        { id: 2, name: "Gâteau Patate", price: "15€"},
+        { id: 3, name: "Bonbon Coco", price: "18€"},
+        { id: 4, name: "Pâté Créole", price: "25€"}
+    ]
+</script>
+
+<section>
+    <h1>Nos gâteaux</h1>
+    <div>
+        <div>
+            {#each gateaux as gateau}
+            <div>
+                <h2>{gateau.name }</h2>
+                <p>{gateau.price }</p>
+            </div>
+            {/each}
+        </div>
+    </div>
+</section>
